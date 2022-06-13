3b96f452e78939dd10d86b65111bc7881ff88ded
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-13 16:44:05 +0530

    Update navMenu.js

---
+++ js/navMenu.js
@@ -0,0 +1,15 @@
+// window.addEventListener("scroll", activeMenu);
+
+// function activeMenu() {
+//     var scrollTop = document.getElementById("KnowUsmDiv").scrollTop();
+
+//     if (scrollTop) {
+//         console.log(scrollTop);
+//     }
+
+// const aboutUs = document.getElementById();
+
+// if (window.scrollY >= 770 && window.scrollY <=3175.333251953125) {
+//     console.log(window.scrollY);
+// }
+// }
