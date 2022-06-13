75c5da6578516d81b4e8bf56502c217b2b81a841
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-13 19:20:49 +0530

    Update navMenu.js

--- js/navMenu.js
+++ js/navMenu.js
@@ -16,6 +16,7 @@ function navHighlighter() {
                 document.getElementById("KnowUsATag").innerHTML = ` <li>
                 <p class="active">About Us</p>
             </li>`;
+                // document.getElementById("KnowUsMobo").innerHTML = ``;
             } else if (current.getAttribute("id") !== "KnowUs") {
                 document.getElementById("KnowUsATag").innerHTML = `<li>About Us</li>`;
             }
