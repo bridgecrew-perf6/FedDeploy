c8c45cb141c3b83f0fb97b4b3639c48bb1ffaa4d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-15 20:51:40 +0530

    Update contactUs.js

--- js/contactUs.js
+++ js/contactUs.js
@@ -5,12 +5,15 @@ const form = document.forms["google-sheet"];
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    document.getElementById("send").innerHTML = "Loading...";
     fetch(scriptURL, {
             method: "POST",
             body: new FormData(form),
         })
-        .then((response) =>
-            alert("Thanks for Contacting us..! We Will Contact You Soon...")
-        )
+        .then((response) => {
+            document.getElementById("send").innerHTML = "Done";
+            alert("Thanks for Contacting us..! We Will Contact You Soon...");
+        })
         .catch((error) => console.error("Error!", error.message));
 });
