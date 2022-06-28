fc6583fff564b5c82faaf265e667d90ce83f1350
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-29 01:11:02 +0530

    Create Event.js

---
+++ js/Event.js
@@ -0,0 +1,64 @@
+const sections = document.querySelectorAll("section[id]");
+
+let path = window.location.pathname;
+
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+    let scrollY = window.pageYOffset;
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+
+        const sectionTop =
+            current.getBoundingClientRect().top + window.pageYOffset - 50;
+        sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            if (
+                current.getAttribute("id") === "ContactUs" ||
+                current.getAttribute("id") === "ContactUsMobo"
+            ) {
+                // console.log("Contact US");
+
+                document.getElementById("ContactUsATag").innerHTML = `<li>
+                <p class="active">Contact Us</p>
+            </li>`;
+
+                document.getElementById(
+                    "ContactUsATagMobo"
+                ).innerHTML = `<p class="active">Contact Us</p>`;
+
+                document.getElementById(
+                    "HomeActive"
+                ).innerHTML = ` <p id="HomeActive">Home</p>`;
+
+                document.getElementById(
+                    "HomeActiveMobo"
+                ).innerHTML = ` <p id="HomeActive">Home</p>`;
+            } else if (
+                current.getAttribute("id") !== "ContactUs" ||
+                current.getAttribute("id") !== "ContactUsMobo"
+            ) {
+                document.getElementById("KnowUsATag").innerHTML = `<li>About Us</li>`;
+
+                document.getElementById("KnowUsMobo").innerHTML = `<p>About Us</p>`;
+
+                document.getElementById(
+                    "ContactUsATag"
+                ).innerHTML = `<li>Contact Us</li>`;
+
+                document.getElementById(
+                    "ContactUsATagMobo"
+                ).innerHTML = `<p>Contact Us</p>`;
+
+                document.getElementById(
+                    "HomeActive"
+                ).innerHTML = ` <p class="active" id="HomeActive">Home</p>`;
+
+                document.getElementById(
+                    "HomeActiveMobo"
+                ).innerHTML = ` <p class="active" id="HomeActive">Home</p>`;
+            }
+        } else {}
+    });
+}
