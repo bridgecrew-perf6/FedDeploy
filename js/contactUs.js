0148db8251007885087cfd8ebd7dc31ce7e0f30b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-29 08:09:14 +0530

    Update contactUs.js

--- js/contactUs.js
+++ js/contactUs.js
@@ -40,6 +40,12 @@ form2.addEventListener("submit", (e) => {
     ).innerHTML = ` <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" id="svgSpinner">
     <circle cx="50" cy="50" r="46"/>
   </svg>`;
+
+    document.getElementById(
+        "ContactUsBtnMobo"
+    ).innerHTML = ` <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" id="svgSpinner">
+<circle cx="50" cy="50" r="46"/>
+</svg>`;
     fetch(scriptURL2, {
             method: "POST",
             body: new FormData(form2),
@@ -51,6 +57,9 @@ form2.addEventListener("submit", (e) => {
             }, 3000);
             document.getElementById(
                 "ContactUsBtn"
+            ).innerHTML = `<img src="./Img/Group 19.png" alt="" id="sendDoneImg">`;
+            document.getElementById(
+                "ContactUsBtnMobo"
             ).innerHTML = `<img src="./Img/Group 19.png" alt="" id="sendDoneImg">`;
         })
         .catch((error) => console.error("Error!", error.message));
